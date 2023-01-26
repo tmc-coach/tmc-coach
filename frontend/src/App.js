e45fcb4b91ca6e79dc47428e28df4cf38bac115b
Anna Suuronen <annasuuronen@icloud.com>
2023-01-26 14:50:34 +0200

    Login form added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,31 @@
 import './App.css'
-import React, { useState, useEffect } from 'react'
+//import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 function App() {
-  const [link, setLink] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-  useEffect(() => {
-    fetch('http://localhost:5000/techdemo')
-      .then(res => res.json())
-      .then(data => setLink(data.link))
-  }, [])
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <a href={link}>Backend says hi!</a>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            Username:
+            <input type="text" value={username} name="username" onChange={({ target }) => setUsername(target.value)} />
+          </div>
+          <div>
+            Password:
+            <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+          </div>
+          <button type="submit">Login</button>
+        </form>
       </header>
     </div>
   )
