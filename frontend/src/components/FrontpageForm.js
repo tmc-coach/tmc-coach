5780ef876b361220c87fbdd0bb768d7702add380
Anna Suuronen <annasuuronen@icloud.com>
2023-01-31 14:27:33 +0200

    Frontpageform added to components

---
+++ frontend/src/components/FrontpageForm.js
@@ -0,0 +1,11 @@
+const FrontpageForm = () => {
+  return (
+    <div>
+      <h1>TMC-Coach frontpage</h1>
+      <h2>Your courses</h2>
+      <p>Add new course</p>
+    </div>
+  )
+}
+
+export default FrontpageForm
