aa5a8beb1062ac05df5f958be9f4397c12fcfeb9
karppienkingi <mirka.vaananen@gmail.com>
2023-02-08 14:22:41 +0200

    add filter functionality

--- frontend/src/components/Organizations.js
+++ frontend/src/components/Organizations.js
@@ -3,24 +3,21 @@ import missing from '../assets/missing.png'
 
 const Organizations = ({ organizations }) => (
   <div>
-    <h1 className='text-3xl font-medium text-center tracking-wide p-10'>All organizations</h1>
-    <div>
-      {organizations
-        .sort((a, b) => a.name.localeCompare(b.name))
-        .map(org =>
-          <Link to={`/orgs/${org.slug}`} key={org.slug}>
-            <div className='flex border p-5 m-5 border rounded mx-10 hover:bg-stone-100' key={org.slug}>
-              <div className='flex shrink-0 items-center w-16 object-center'>
-                <img width={50} height={60} src={org.logo_path !== 'missing.png' ? `https://tmc.mooc.fi${org.logo_path}` : missing} />
-              </div>
-              <div className="flex-initial w-100 px-2">
-                <h2 className='text-lg font-medium'>{org.name}</h2>
-                {org.information !=='' && <p className="text-base pt-1">{org.information}</p>}
-              </div>
+    {organizations
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .map(org =>
+        <div key={org.slug}>
+          <Link to={`/orgs/${org.slug}`} className='flex border-gray-300 p-5 m-5 border rounded mx-10 hover:bg-stone-100'>
+            <div className='flex shrink-0 items-center w-16 object-center'>
+              <img width={50} height={60} src={org.logo_path !== 'missing.png' ? `https://tmc.mooc.fi${org.logo_path}` : missing} />
+            </div>
+            <div className="flex-initial w-100 px-2">
+              <h2 className='text-lg font-medium'>{org.name}</h2>
+              {org.information !== '' && <p className="text-base pt-1">{org.information}</p>}
             </div>
           </Link>
-        )}
-    </div>
+        </div>
+      )}
   </div>
 )
 
