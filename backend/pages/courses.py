d11efea2c1f9f1198079ecba907afebaa0c0ae14
Tiina <tii.kui12na@gmail.com>
2023-02-10 14:11:22 +0200

    fixes and now should work

--- backend/pages/courses.py
+++ backend/pages/courses.py
@@ -7,9 +7,9 @@ courses = Blueprint("courses", __name__)
 @courses.route("/<org_id>", methods=["GET"])
 def get_course(org_id):
     org_slug = org_id
-    print("ok1")
-    response = requests.get(f"https://tmc.mooc.fi/api/v8/core/org/{org_slug}/courses.json", headers={"Accept": "application/json"})
-    print("ok")
+    response = requests.get(
+        f"https://tmc.mooc.fi/api/v8/core/org/{org_slug}/courses.json", headers={"Accept": "application/json"}
+        )
     if response.status_code == 403:
         return jsonify(error="Forbidden"), 403
     if response.status_code == 404:
