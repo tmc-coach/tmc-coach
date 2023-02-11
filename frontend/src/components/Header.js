b3ec29aeafc3015a827fc4b3c55a804c0a1c2373
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2023-02-11 20:22:12 +0200

    signout button doesnt show up on login page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,15 @@
 import Logout from './LogOut'
+import { useLocation } from 'react-router-dom'
+
 const Header = () => {
+  const location = useLocation()
+  if (location.pathname==='/login') {
+    return (
+      <div className="flex justify-between items-center p-2 bg-blue-600">
+        <h2 className="text-white">TMC Coach</h2>
+      </div>
+    )
+  }
   return (
     <div className="flex justify-between items-center p-2 bg-blue-600">
       <h2 className="text-white">TMC Coach</h2>
