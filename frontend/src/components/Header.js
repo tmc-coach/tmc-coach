e615a88061da134b739061a8a9049e5368cb05e0
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2023-02-11 20:26:24 +0200

    TMC Coach text in the banner turned into a link to '/' route

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,18 @@
 import Logout from './LogOut'
-import { useLocation } from 'react-router-dom'
+import { useLocation, Link } from 'react-router-dom'
 
 const Header = () => {
   const location = useLocation()
   if (location.pathname==='/login') {
     return (
       <div className="flex justify-between items-center p-2 bg-blue-600">
-        <h2 className="text-white">TMC Coach</h2>
+        <Link className="text-white" to="/">TMC Coach</Link>
       </div>
     )
   }
   return (
     <div className="flex justify-between items-center p-2 bg-blue-600">
-      <h2 className="text-white">TMC Coach</h2>
+      <Link className="text-white" to="/">TMC Coach</Link>
       <Logout/>
     </div>
   )
