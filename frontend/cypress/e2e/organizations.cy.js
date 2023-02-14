8a75f6086e7d8d1dbdd01f9bde9285a50f521b40
Anna Suuronen <annasuuronen@icloud.com>
2023-02-14 14:19:54 +0200

    indentation in organizations.cy.js-file fixed

--- frontend/cypress/e2e/organizations.cy.js
+++ frontend/cypress/e2e/organizations.cy.js
@@ -1,32 +1,32 @@
 /// <reference types="Cypress" />
 
 describe('TMC-Coach login', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:3000')
-    })
-    it('logged in user can go to the organization page', () => {
-      cy.get('input[name=username]').type(Cypress.env('tmcusername'))
-      cy.get('input[name=password]').type(Cypress.env('tmcpassword'))
-      cy.get('button[type=submit]').click()
-      cy.url().should('not.include', '/login')
-      cy.contains('Organizations').click()
-      cy.url().should('include', '/orgs')
-    })
-    it('logged in user can click a organization', () => {
-      cy.get('input[name=username]').type(Cypress.env('tmcusername'))
-      cy.get('input[name=password]').type(Cypress.env('tmcpassword'))
-      cy.get('button[type=submit]').click()
-      cy.url().should('not.include', '/login')
-      cy.contains('Organizations').click()
-      cy.url().should('include', '/orgs')
-      cy.contains('Aalto BIZ').click()
-      cy.url().should('include', '/orgs/aalto-biz')
-    })
-    it('user can search organizations on searchbar', () => {
-      cy.visit('http://localhost:3000/orgs')
-      cy.get('input[type=search]').type('mooc')
-      cy.contains('Aalto BIZ').should('not.exist')
-      cy.contains('MOOC').click()
-      cy.url().should('include', '/orgs/mooc')
-    })
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
   })
+  it('logged in user can go to the organization page', () => {
+    cy.get('input[name=username]').type(Cypress.env('tmcusername'))
+    cy.get('input[name=password]').type(Cypress.env('tmcpassword'))
+    cy.get('button[type=submit]').click()
+    cy.url().should('not.include', '/login')
+    cy.contains('Organizations').click()
+    cy.url().should('include', '/orgs')
+  })
+  it('logged in user can click a organization', () => {
+    cy.get('input[name=username]').type(Cypress.env('tmcusername'))
+    cy.get('input[name=password]').type(Cypress.env('tmcpassword'))
+    cy.get('button[type=submit]').click()
+    cy.url().should('not.include', '/login')
+    cy.contains('Organizations').click()
+    cy.url().should('include', '/orgs')
+    cy.contains('Aalto BIZ').click()
+    cy.url().should('include', '/orgs/aalto-biz')
+  })
+  it('user can search organizations on searchbar', () => {
+    cy.visit('http://localhost:3000/orgs')
+    cy.get('input[type=search]').type('mooc')
+    cy.contains('Aalto BIZ').should('not.exist')
+    cy.contains('MOOC').click()
+    cy.url().should('include', '/orgs/mooc')
+  })
+})
