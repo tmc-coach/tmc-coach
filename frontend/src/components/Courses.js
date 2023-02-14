6f3ee75bc8697cb0df7f1a0130cd272c49406f4c
Väänänen Mirka M <mirvaana@cubbli20-gold.local>
2023-02-14 16:48:11 +0200

    fixes to UI

--- frontend/src/components/Courses.js
+++ frontend/src/components/Courses.js
@@ -5,12 +5,12 @@ const Courses = ({ courses }) => (
       .sort((a, b) => a.name.localeCompare(b.name))
       .map(course =>
         <div key={course.id} className='border-black p-5 m-5 border rounded mx-10'>
-          <h4 className=''>
+          <div className='text-2xl font-medium'>
             {course.title}
-          </h4>
-          <br></br>
-          {course.description !== '' ? course.description : 'A mysterious course without description.'}
-
+          </div>
+          <div>
+            {course.description !== '' ? course.description : 'A mysterious course without description.'}
+          </div>
         </div>)}
   </div>
 )
--- frontend/src/routes/CoursesPage.js
+++ frontend/src/routes/CoursesPage.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import Courses from '../components/Courses'
 import courseService from '../services/courses'
-import { useParams } from 'react-router-dom'
+import Filter from '../components/Filter'
 
 const Courses_Page = () => {
 
   const [courses, setCourses] = useState([])
+  const [filter, setFilter] = useState('')
 
   const org_slug = useParams().slug
 
@@ -13,10 +15,15 @@ const Courses_Page = () => {
     courseService.get_courses(org_slug).then(courses => setCourses(courses))
   }, [])
 
+  const coursesToShow = (filter.length === 0) ? courses : courses.filter(course => course.title.toLowerCase().includes(filter.toLowerCase()))
+
   return (
-    <div>
-      <h1 className='text-3xl font-medium text-center tracking-wide p-10'>Courses</h1>
-      <Courses courses={courses} />
+    <div className='flex justify-center'>
+      <div className='flex justify-center flex-col w-100 max-w-screen-lg'>
+        <h1 className='text-3xl font-medium text-center tracking-wide p-10'>Courses</h1>
+        {courses.length >= 2 && <Filter value={filter} handleChange={({ target }) => setFilter(target.value)} />}
+        {courses.length > 0 ? <Courses courses={coursesToShow} /> : 'No available courses'}
+      </div>
     </div>
   )
 
