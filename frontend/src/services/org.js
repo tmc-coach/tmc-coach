6c35a4fb69155c6a772ea386b028828e0365294f
Tuukka Laitinen <8557415+tuukkalai@users.noreply.github.com>
2023-02-19 21:58:46 +0200

    Clean up formatting errors

--- frontend/src/services/org.js
+++ frontend/src/services/org.js
@@ -1,21 +1,21 @@
-import axios from "axios";
+import axios from 'axios'
 
-const prod = process.env.NODE_ENV === "production";
-const baseUrl = prod ? "/org/" : process.env.REACT_APP_BASEURL + "/org/";
+const prod = process.env.NODE_ENV === 'production'
+const baseUrl = prod ? '/org/' : process.env.REACT_APP_BASEURL + '/org/'
 
 const get_orgs = async () => {
   const response = await axios.get(baseUrl, {
-    headers: { Authorization: localStorage.getItem("user") },
-  });
-  return response.data;
-};
+    headers: { Authorization: localStorage.getItem('user') },
+  })
+  return response.data
+}
 
 const get_org = async (org_slug) => {
   const response = await axios.get(baseUrl, {
-    headers: { Authorization: localStorage.getItem("user") },
-  });
-  const org = response.data.filter((org) => org.slug === org_slug);
-  return org;
-};
+    headers: { Authorization: localStorage.getItem('user') },
+  })
+  const org = response.data.filter((org) => org.slug === org_slug)
+  return org
+}
 
-export default { get_orgs, get_org };
+export default { get_orgs, get_org }
