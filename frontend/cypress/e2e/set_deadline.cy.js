ccb7d0f098fb2c24e7d6254f7c271b5e71246efc
Anna Suuronen <annasuuronen@icloud.com>
2023-02-21 14:27:04 +0200

    cypress test for going to the set deadline page

---
+++ frontend/cypress/e2e/set_deadline.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+
+describe('TMC-Coach set deadline', { defaultCommandTimeout: 8000 }, () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+    it('logged in user can go to the set deadline page', () => {
+      cy.get('input[name=username]').type(Cypress.env('tmcusername'))
+      cy.get('input[name=password]').type(Cypress.env('tmcpassword'))
+      cy.get('button[type=submit]').click()
+      cy.url().should('not.include', '/login')
+      cy.visit('http://localhost:3000/orgs')
+      cy.visit('http://localhost:3000/orgs/mooc')
+      cy.visit('http://localhost:3000/orgs/courses/277/set_deadline')
+      cy.contains('Set deadline for course 2013 Object-oriented programming, part 1').should('exist')
+      cy.contains('Choose the deadline for this course').should('exist')
+  })
+})
