38e39201bd89fc2d1c722ff4ea7ee81786a6cd3a
Anna Suuronen <annasuuronen@icloud.com>
2023-02-22 00:32:20 +0200

    tried to do some backend tests

--- backend/pages/deadline.py
+++ backend/pages/deadline.py
@@ -33,6 +33,7 @@ def get_deadlines(username):
 # Database model demo
 class deadlines(db.Model):
     __tablename__ = 'deadlines'
+    __table_args__ = {'extend_existing': True} 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
     course_id = db.Column(db.Integer, nullable=False)
