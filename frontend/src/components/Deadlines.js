c24067841f68940f6cc9bd9b9a4a4ef48f18d19b
Anna Suuronen <annasuuronen@icloud.com>
2023-02-23 17:03:38 +0200

    set_deadline-page's components are in the middle of the page

--- frontend/src/components/Deadlines.js
+++ frontend/src/components/Deadlines.js
@@ -8,16 +8,16 @@ const Deadlines = ({ deadlines, course_id }) => {
   if (deadlinesToShow.length === 0) {
     return (
       <div>
-        <p>No deadlines chosen for this course</p>
+        <p className="flex justify-center px-5 m-5">No deadlines chosen for this course</p>
       </div>
     )
   }
   return (
     <div>
-      <p>Deadlines chosen for this course</p>
+      <p className="flex justify-center px-5 m-5">Deadlines chosen for this course</p>
       {deadlinesToShow
         .map(deadline =>
-          <div key={deadline.id}>
+          <div key={deadline.id} className="flex justify-center px-5 m-5">
             {deadline.date}
           </div>
         )
