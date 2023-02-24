bdc729d2867974a55f549c0c5ee4bf2b754254d1
AnonymousHobbit <30089493+AnonymousHobbit@users.noreply.github.com>
2023-02-24 18:50:54 +0200

    cleaning up services

--- backend/pages/courses.py
+++ backend/pages/courses.py
@@ -5,6 +5,21 @@ import json
 
 courses = Blueprint("courses", __name__)
 
+@courses.route("/<course_id>", methods=["GET"])
+def get_course_info(course_id):
+    auth_header = request.headers.get("Authorization", None)
+    if not auth_header:
+        return jsonify(error="Authorization header missing")
+    token = decode_jwt(auth_header)
+    response = requests.get(
+        f"https://tmc.mooc.fi/api/v8/core/courses/{course_id}", headers={"Accept": "application/json", "Authorization": token["token"]}
+    )
+    if response.status_code == 403:
+        return jsonify(error="Forbidden"), 403
+    if response.status_code == 404:
+        return jsonify(error="Not Found"), 404
+    return response.json()
+
 
 @courses.route("/<course_id>/exercises", methods=["GET"])
 def get_exercises(course_id):
--- frontend/src/services/courses.js
+++ frontend/src/services/courses.js
@@ -6,8 +6,6 @@ const baseUrl = prod ? '/api/courses' : process.env.REACT_APP_BASEURL + '/course
 const get_course_info = async course_id => {
 
   const response = await axios.get(`${baseUrl}/${course_id}`, { headers: { Authorization: localStorage.getItem('token') } })
-
-
   return response.data
 }
 
