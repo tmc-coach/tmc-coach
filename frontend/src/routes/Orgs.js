2c128706e6977b7203d39fc7124dbe67c365a4d0
karppienkingi <mirka.vaananen@gmail.com>
2023-02-28 16:12:28 +0200

    add tailwind changes

--- frontend/src/routes/Orgs.js
+++ frontend/src/routes/Orgs.js
@@ -23,10 +23,11 @@ const Orgs = () => {
   }, [])
 
   const organizationsToShow = (filter.length === 0) ? orgs : orgs.filter(org => org.name.toLowerCase().includes(filter.toLowerCase()))
+
   return (
-    <div>
-      <FrequentlyUsedOrgs orgs={orgs} />
-      <div>
+    <div className='flex justify-center mx-5'>
+      <div className='flex justify-center flex-col max-w-screen-lg'>
+        <FrequentlyUsedOrgs orgs={orgs} />
         <h1 className='text-3xl font-medium text-center tracking-wide p-10'>All organizations</h1>
         <Filter value={filter} handleChange={({ target }) => setFilter(target.value)} />
         <Organizations organizations={organizationsToShow} />
