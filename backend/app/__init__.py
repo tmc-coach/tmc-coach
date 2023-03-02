543730c580a54ef404fbf5061abf7148425159cb
János Pap <murmurian@gmail.com>
2023-03-02 14:34:37 +0200

    Update docker settings

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,10 +3,8 @@ from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 db = SQLAlchemy()
-migrate = Migrate()
 
 def create_app():
     # create Flask app instance
@@ -27,7 +25,6 @@ def create_app():
     # set up database connection
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     db.init_app(app)
-    migrate.init_app(app, db)
 
     # enable CORS
     CORS(app)
--- backend/tmc-coach.py
+++ backend/tmc-coach.py
@@ -2,14 +2,13 @@ from flask_migrate import Migrate, upgrade
 from app import create_app, db
 from app.models import deadlines, TargetDate
 
-
 app = create_app()
 migrate = Migrate(app, db)
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {"db": db, "deadlines": deadlines, "TargetDate": TargetDate}
+    return {"deadlines": deadlines, "TargetDate": TargetDate}
 
 
 if __name__ == "__main__":
