742f5cf6bd2d0bd1cdbe548d4b02ad8e663a5ebd
Anna Suuronen <annasuuronen@icloud.com>
2023-03-06 19:09:27 +0200

    database model deleted from deadline.py

--- backend/pages/deadline.py
+++ backend/pages/deadline.py
@@ -39,15 +39,3 @@ def get_deadlines():
     
     deadlines = get_deadlines_function(username)
     return deadlines
-
-# Database model demo
-class deadlines(db.Model):
-    __tablename__ = 'deadlines'
-    __table_args__ = {'extend_existing': True} 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
-    course_id = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
-
-    def __repr__(self):
-        return f"deadlines('{self.date}')"
