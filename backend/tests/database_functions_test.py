8592a6fb396953574a4f250d5ce4dfaec9f703b5
Anna Suuronen <annasuuronen@icloud.com>
2023-03-06 19:11:49 +0200

    python test for setting deadline

--- backend/tests/database_functions_test.py
+++ backend/tests/database_functions_test.py
@@ -2,17 +2,14 @@ import unittest
 from unittest import TestCase
 import json
 import os
-from flask import Flask
-from flask_cors import CORS
-#from flask_testing import TestCase
 from app import db
 from sqlalchemy.sql import text
 from datetime import date
 from dotenv import load_dotenv
-import requests
-import app
 from database_functions.get_deadlines import get_deadlines_function
-from flask_sqlalchemy import SQLAlchemy
+from database_functions.set_deadline import set_deadline_function
+import datetime
+
 
 
 class DeadlinesTestCase(TestCase):
@@ -28,5 +25,38 @@ class DeadlinesTestCase(TestCase):
                 self.assertEqual(deadline["username"], username)
             except:
                 break
+
+    def test_set_deadlines_adds_a_deadline_to_database(self):
+        username = os.getenv("TMCUSERNAME")
+        deadlines = get_deadlines_function(username)
+        course_id = 1169
+
+        date_for_deadline = datetime.datetime(2025, 2, 18)
+
+        sql = "SELECT * FROM deadlines WHERE date=:date"
+        result = db.session.execute(text(sql), {"date":date_for_deadline})
+        deadlines = result.fetchall() 
+
+        self.assertEqual(len(deadlines), 0)  
+
+        set_deadline_function(username, date_for_deadline, course_id)
+
+        sql = "SELECT * FROM deadlines WHERE date=:date"
+        result = db.session.execute(text(sql), {"date":date_for_deadline})
+        deadlines = result.fetchall() 
+
+        self.assertEqual(len(deadlines), 1) 
+
+        sql = "DELETE FROM deadlines WHERE date=:date"
+        result = db.session.execute(text(sql), {"date":date_for_deadline})
+        db.session.commit()
+
+        sql = "SELECT * FROM deadlines WHERE date=:date"
+        result = db.session.execute(text(sql), {"date":date_for_deadline})
+        deadlines = result.fetchall()
+ 
+        self.assertEqual(len(deadlines), 0)  
         
-        #self.assertEqual(dictionary, "yeet")
+        
+
+
