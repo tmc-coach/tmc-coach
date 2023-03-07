0ee64ab40455791b11bf0efe1a1541f923651c94
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2023-03-07 14:43:47 +0200

    fixed naming

--- frontend/cypress/e2e/header_and_signout.cy.js
+++ frontend/cypress/e2e/header_and_signout.cy.js
@@ -8,40 +8,40 @@ describe('TMC-Coach header', { defaultCommandTimeout: 8000 }, () => {
     beforeEach(() => {
       cy.restoreLocalStorage()
     })
-    it('on organizations page can click header link to get to home page', () => {
+    it('on organizations page can click header link to get to homepage', () => {
       cy.orgspage()
       cy.contains('TMC Coach').click()
       cy.url().should('not.include', '/orgs')
     })
-    it('on course page can click header link to get to home page', () => {
+    it('on course page can click header link to get to homepage', () => {
       cy.coursepage()
       cy.contains('TMC Coach').click()
       cy.url().should('not.include', '/orgs/aalto-biz')
     })
-    it('can use sign out button and is directed to loginpage on homepage', () => {
+    it('on homepage can use sign out button and is directed to login page', () => {
       cy.homepage()
       cy.contains('Sign out').click()
       cy.url().should('include', '/login')
     })
-    it('can use sign out button and is directed to loginpage on organizationspage', () => {
+    it('on organizations page can use sign out button and is directed to login page', () => {
       cy.orgspage()
       cy.contains('Sign out').click()
       cy.url().should('include', '/login')
     })
-    it('can use sign out button and is directed to loginpage on coursepage', () => {
+    it('on course page can use sign out button and is directed to login page', () => {
       cy.coursepage()
       cy.url().should('include', '/orgs/aalto-biz')
       cy.contains('Sign out').click()
       cy.url().should('include', '/login')
     })
-    it('can use sign out button and is directed to loginpage from courses exercises page', () => {
+    it('on courses exercises page can use sign out button and is directed to login page', () => {
       cy.visit('http://localhost:3000/orgs/courses/900')
       cy.contains('Sign out').click()
       cy.url().should('include', '/login')
     })
   })
   context('logged out user', () => {
-    it('can not see sign out button on login page', () => {
+    it('on login page can not see sign out button', () => {
       cy.loginpage()
       cy.contains('Sign out').should('not.exist')
     })
