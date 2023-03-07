19f4e2add3a53ad939ad5cb8e7dd5c66dbedad6e
Anna Suuronen <annasuuronen@icloud.com>
2023-03-07 14:21:58 +0200

    backend tests for database functions fixed

--- backend/tests/database_functions_test.py
+++ backend/tests/database_functions_test.py
@@ -14,20 +14,22 @@ import datetime
 
 class DeadlinesTestCase(TestCase):
     def test_get_deadlines_only_gets_deadlines_that_have_been_made_by_the_user(self):
-        username = os.getenv("TMCUSERNAME")
-        deadlines = get_deadlines_function(username)
+        #username = os.getenv("TMCUSERNAME")
+        user_id = 619104
+        deadlines = get_deadlines_function(user_id)
         dictionary = json.loads(deadlines)
 
         i = 0
         while True:
             try:
                 deadline = dictionary[i]
-                self.assertEqual(deadline["username"], username)
+                self.assertEqual(deadline["user_id"], user_id)
             except:
                 break
 
     def test_set_deadlines_adds_a_deadline_to_database(self):
-        username = os.getenv("TMCUSERNAME")
+        #username = os.getenv("TMCUSERNAME")
+        user_id = 619104
         course_id = 1169
         date_for_deadline = datetime.datetime(2025, 2, 18)
 
@@ -37,7 +39,7 @@ class DeadlinesTestCase(TestCase):
 
         self.assertEqual(len(deadlines), 0)
 
-        set_deadline_function(username, date_for_deadline, course_id)
+        set_deadline_function(user_id, date_for_deadline, course_id)
 
         sql = "SELECT * FROM deadlines WHERE date=:date"
         result = db.session.execute(text(sql), {"date":date_for_deadline})
@@ -56,12 +58,13 @@ class DeadlinesTestCase(TestCase):
         self.assertEqual(len(deadlines), 0)  
 
     def test_set_deadline_has_created_at_column(self):
-        username = os.getenv("TMCUSERNAME")
+        #username = os.getenv("TMCUSERNAME")
+        user_id = 619104
         course_id = 1169
         date_for_deadline = datetime.datetime(2028, 5, 27)
         date_now = datetime.datetime.now().date()
 
-        set_deadline_function(username, date_for_deadline, course_id)
+        set_deadline_function(user_id, date_for_deadline, course_id)
 
         sql = "SELECT * FROM deadlines WHERE date=:date AND created_at=:created_at"
         result = db.session.execute(text(sql), {"date":date_for_deadline, "created_at":date_now})
@@ -72,4 +75,3 @@ class DeadlinesTestCase(TestCase):
         sql = "DELETE FROM deadlines WHERE date=:date AND created_at=:created_at"
         result = db.session.execute(text(sql), {"date":date_for_deadline, "created_at":date_now})
         db.session.commit()
-        
