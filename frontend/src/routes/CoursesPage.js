0492c9b3d32ff763a99b53d55f87582e3fbd9c96
karppienkingi <mirka.vaananen@gmail.com>
2023-03-13 18:26:08 +0200

    create a component for errorelement and initial try to get it to work

--- frontend/src/routes/CoursesPage.js
+++ frontend/src/routes/CoursesPage.js
@@ -4,22 +4,23 @@ import orgService from '../services/org'
 import Organization from '../components/Organization'
 import Filter from '../components/Filter'
 import Courses from '../components/Courses'
-import Loading from '../components/Loading'
 
 const Courses_Page = () => {
 
-  const [org, setOrg] = useState({
-    name: '',
-    logo_path: '',
-    information: ''
-  })
+  // const [org, setOrg] = useState({
+  //   name: '',
+  //   logo_path: '',
+  //   information: ''
+  // })
+  const [org, setOrg] = useState([])
   const [courses, setCourses] = useState([])
   const [filter, setFilter] = useState('')
 
   const org_slug = useParams().slug
 
   useEffect(() => {
-    orgService.get_org(org_slug).then(org => setOrg({ ...org, name: org[0].name, information: org[0].information, logo_path: org[0].logo_path }))
+    //orgService.get_org(org_slug).then(org => setOrg({ ...org, name: org[0].name, information: org[0].information, logo_path: org[0].logo_path }))
+    orgService.get_org(org_slug).then(org => setOrg(org))
     orgService.get_courses(org_slug).then(courses => setCourses(courses))
   }, [])
 
@@ -27,15 +28,12 @@ const Courses_Page = () => {
 
   return (
     <div className='flex justify-center mx-5'>
-      {courses.length > 0 ? (<div className='flex justify-center flex-col w-100 max-w-screen-lg text-center'>
+      <div className='flex justify-center flex-col w-100 max-w-screen-lg text-center'>
         <Organization organization={org} />
         <h1 className='text-3xl font-medium text-center tracking-wide px-10 pb-10'>Courses</h1>
         {courses.length >= 2 && <Filter value={filter} handleChange={({ target }) => setFilter(target.value)} />}
-        <Courses courses={coursesToShow} />
-      </div>) : (
-        <Loading />
-      )
-      }
+        {courses.length > 0 ? <Courses courses={coursesToShow} /> : 'No available courses'}
+      </div>
     </div>
   )
 }
--- frontend/src/services/org.js
+++ frontend/src/services/org.js
@@ -11,11 +11,12 @@ const get_orgs = async () => {
 }
 
 const get_org = async (org_slug) => {
-  const response = await axios.get(`${baseUrl}/`, {
+  const response = await axios.get(`${baseUrl}/${org_slug}`, {
     headers: { Authorization: localStorage.getItem('user') },
   })
-  const org = response.data.filter((org) => org.slug === org_slug)
-  return org
+  //const org = response.data.filter((org) => org.slug === org_slug)
+  console.log(response.data)
+  return response.data
 }
 
 const get_courses = async (org_slug) => {
