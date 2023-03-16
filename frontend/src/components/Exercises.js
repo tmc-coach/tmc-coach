92707edd71a5a49d9fd86359583c6f97e5b8cc8d
AnonymousHobbit <30089493+AnonymousHobbit@users.noreply.github.com>
2023-03-16 22:51:57 +0200

    redirect to orgs

--- frontend/src/components/Exercises.js
+++ frontend/src/components/Exercises.js
@@ -3,27 +3,26 @@ import Deadlines from './Deadlines'
 
 const Exercises = ({ exercises, course_id }) => (
   <div>
-
-      <div key={exercises[0].course_id}>
+    <div key={exercises[0].course_id}>
       <h1 className='text-3xl font-medium text-center tracking-wide p-10'>{exercises[0].course_title}</h1>
       {exercises[0].maximum_exercises > -1 ? (
-          <>
-            <div className='text-2xl font-medium'>
-              Awarded points: {exercises[0].completed_percentage} %
-            </div>
-            <br></br>
-            <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+        <>
+          <div className='text-2xl font-medium'>
+            Awarded points: {exercises[0].completed_percentage} %
+          </div>
+          <br></br>
+          <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
             <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${(exercises[0].completed_percentage < 5) ? 5 : exercises[0].completed_percentage}` + '%' }} >{exercises[0].completed_percentage} %</div>
-            </div>
-            <br></br>
-            <div>
-              <Deadlines course_id={course_id} />
-            </div>
-          </>
-        ) : (
-          <p>No exercises on this course</p>
-        )}
-      </div>
+          </div>
+          <br></br>
+          <div>
+            <Deadlines course_id={course_id} />
+          </div>
+        </>
+      ) : (
+        <p>No exercises on this course</p>
+      )}
+    </div>
   </div>
 )
 
