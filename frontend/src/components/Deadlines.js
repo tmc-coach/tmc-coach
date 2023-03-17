d89a711eb62f5db493d509343107c4099bcbcd56
Tiina <tii.kui12na@gmail.com>
2023-03-17 14:20:51 +0200

    UI fixes

--- frontend/src/components/Deadlines.js
+++ frontend/src/components/Deadlines.js
@@ -9,11 +9,13 @@ const Deadlines = ({ deadlines, onDelete }) => {
 
   return (
     <div>
-      <p className="flex justify-center px-5 m-5">Deadlines chosen for this course</p>
+      <p className="flex justify-center px-5 m-5 text-2xl font-medium">Deadline chosen for this course:</p>
       <div key={deadlines.id} className="flex justify-center px-5 m-5">
         {deadlines.date}
       </div>
-      <button onClick={onDelete} value="delete_deadline">Delete</button>
+      <div className="flex justify-center px-5 m-5">
+        <button onClick={onDelete} value="delete_deadline" className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete deadline</button>
+      </div>
     </div>
   )
 }
