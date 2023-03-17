c85201ed54f7b90fbbe42d9cda69114b6d9d23b7
karppienkingi <mirka.vaananen@gmail.com>
2023-03-17 19:22:01 +0200

    add description to loginpage

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,17 +3,23 @@ import { useLocation, Link } from 'react-router-dom'
 
 const Header = () => {
   const location = useLocation()
-  if (location.pathname==='/login') {
+  if (location.pathname === '/login') {
     return (
-      <div className="flex justify-between items-center p-2 bg-blue-600">
-        <Link className="text-white" to="/">TMC Coach</Link>
+      <div>
+        <div className="flex justify-between items-center p-2 bg-blue-600">
+          <Link className="text-white" to="/">TMC Coach</Link>
+        </div>
+        <div className="flex flex-col justify-center bg-blue-600 text-center h-80">
+          <div className="text-white text-4xl font-medium mb-2">TMC-Coach</div>
+          <div className="text-white text-2xl font-medium">Schedule your courses and get feedback on your pace</div>
+        </div>
       </div>
     )
   }
   return (
     <div className="flex justify-between items-center p-2 bg-blue-600">
       <Link className="text-white" to="/">TMC Coach</Link>
-      <Logout/>
+      <Logout />
     </div>
   )
 }
