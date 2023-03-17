999d368442f1142ad92325f219fae735c891e47d
Anna Suuronen <annasuuronen@icloud.com>
2023-03-17 15:09:18 +0200

    backend tests in the database_functions_test-py-file have been fixed

--- backend/tests/database_functions_test.py
+++ backend/tests/database_functions_test.py
@@ -2,21 +2,24 @@ import unittest
 from unittest import TestCase
 import json
 import os
-from app import db
 from sqlalchemy.sql import text
 from datetime import date
 from dotenv import load_dotenv
 from database_functions.deadline_functions import get_deadlines_function
 from database_functions.deadline_functions import set_deadline_function
 import datetime
-
-
+from app import create_app, db
 
 class DeadlinesTestCase(TestCase):
+    def setUp(self):
+        self.app = create_app()
+
     def test_get_deadlines_only_gets_deadlines_that_have_been_made_by_the_user(self):
-        #username = os.getenv("TMCUSERNAME")
         user_id = os.getenv("TMCUSERID")
-        deadlines = get_deadlines_function(user_id)
+
+        with self.app.app_context():
+            deadlines = get_deadlines_function(user_id)
+
         dictionary = json.loads(deadlines)
 
         i = 0
@@ -28,50 +31,58 @@ class DeadlinesTestCase(TestCase):
                 break
 
     def test_set_deadlines_adds_a_deadline_to_database(self):
-        #username = os.getenv("TMCUSERNAME")
         user_id = os.getenv("TMCUSERID")
         course_id = 1169
         date_for_deadline = datetime.datetime(2025, 2, 18)
 
-        sql = "SELECT * FROM deadlines WHERE date=:date"
-        result = db.session.execute(text(sql), {"date":date_for_deadline})
-        deadlines = result.fetchall() 
+        deadlines = []
+
+        with self.app.app_context():
+            sql = "SELECT * FROM deadlines WHERE date=:date"
+            result = db.session.execute(text(sql), {"date":date_for_deadline})
+            deadlines = result.fetchall() 
 
         self.assertEqual(len(deadlines), 0)
 
-        set_deadline_function(user_id, date_for_deadline, course_id)
+        with self.app.app_context():
+            set_deadline_function(user_id, date_for_deadline, course_id)
 
-        sql = "SELECT * FROM deadlines WHERE date=:date"
-        result = db.session.execute(text(sql), {"date":date_for_deadline})
-        deadlines = result.fetchall() 
+        with self.app.app_context():
+            sql = "SELECT * FROM deadlines WHERE date=:date"
+            result = db.session.execute(text(sql), {"date":date_for_deadline})
+            deadlines = result.fetchall() 
 
         self.assertEqual(len(deadlines), 1) 
 
-        sql = "DELETE FROM deadlines WHERE date=:date"
-        result = db.session.execute(text(sql), {"date":date_for_deadline})
-        db.session.commit()
+        with self.app.app_context():
+            sql = "DELETE FROM deadlines WHERE date=:date"
+            result = db.session.execute(text(sql), {"date":date_for_deadline})
+            db.session.commit()
 
-        sql = "SELECT * FROM deadlines WHERE date=:date"
-        result = db.session.execute(text(sql), {"date":date_for_deadline})
-        deadlines = result.fetchall()
+        with self.app.app_context():
+            sql = "SELECT * FROM deadlines WHERE date=:date"
+            result = db.session.execute(text(sql), {"date":date_for_deadline})
+            deadlines = result.fetchall()
  
         self.assertEqual(len(deadlines), 0)  
 
     def test_set_deadline_has_created_at_column(self):
-        #username = os.getenv("TMCUSERNAME")
         user_id = os.getenv("TMCUSERID")
         course_id = 1169
         date_for_deadline = datetime.datetime(2028, 5, 27)
         date_now = datetime.datetime.now().date()
 
-        set_deadline_function(user_id, date_for_deadline, course_id)
+        deadlines = []
 
-        sql = "SELECT * FROM deadlines WHERE date=:date AND created_at=:created_at"
-        result = db.session.execute(text(sql), {"date":date_for_deadline, "created_at":date_now})
-        deadlines = result.fetchall()
+        with self.app.app_context():
+            set_deadline_function(user_id, date_for_deadline, course_id)
 
-        self.assertEqual(len(deadlines), 1)
+            sql = "SELECT * FROM deadlines WHERE date=:date AND created_at=:created_at"
+            result = db.session.execute(text(sql), {"date":date_for_deadline, "created_at":date_now})
+            deadlines = result.fetchall()
 
-        sql = "DELETE FROM deadlines WHERE date=:date AND created_at=:created_at"
-        result = db.session.execute(text(sql), {"date":date_for_deadline, "created_at":date_now})
-        db.session.commit()
+            self.assertEqual(len(deadlines), 1)
+
+            sql = "DELETE FROM deadlines WHERE date=:date AND created_at=:created_at"
+            result = db.session.execute(text(sql), {"date":date_for_deadline, "created_at":date_now})
+            db.session.commit()
