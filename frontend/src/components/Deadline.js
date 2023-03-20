f035c5a326655e6c4f48a9aa547b91d8146f8b4f
Tiina <tii.kui12na@gmail.com>
2023-03-20 15:45:31 +0200

    frontend for deleting a dl

--- frontend/src/components/Deadline.js
+++ frontend/src/components/Deadline.js
@@ -1,4 +1,4 @@
-const Deadline = ({ deadlines }) => {
+const Deadline = ({ deadlines, onDelete }) => {
   if (deadlines.length === 0) {
     return (
       <div>
@@ -10,6 +10,9 @@ const Deadline = ({ deadlines }) => {
   return (
     <div>
       <p key={deadlines.id} className="flex justify-center px-5 m-5">You have planned to complete this course by {deadlines.date}</p>
+      <div className="flex justify-center px-5 m-5">
+        <button onClick={onDelete} value="delete_deadline" className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete deadline</button>
+      </div>
     </div>
   )
 }
