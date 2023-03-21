d12dc829189d148bb243682fdd48522f747d1cb7
Anna Suuronen <annasuuronen@icloud.com>
2023-03-21 14:21:20 +0200

    confirm-window shows up if the user tries to set the deadline under four days away

--- frontend/src/components/Deadlines.js
+++ frontend/src/components/Deadlines.js
@@ -25,6 +25,26 @@ const Deadlines = ({ course_id }) => {
   const handleSetDeadline = async (event) => {
     event.preventDefault()
 
+    console.log(date)
+
+    let created_at = new Date()
+    //created_at.setHours(12)
+    //created_at.setMinutes(0)
+    //created_at.setSeconds(0)
+    //created_at.setMilliseconds(0)
+
+    console.log(created_at)
+
+    const days_between = Math.floor((date - created_at) / (1000 * 60 * 60 *24))
+
+    let text = ''
+    if (days_between < 4) {
+      text = 'Why do you want to set a deadline that is under four days away??? Go do your exercises!!! No checkpoints will be asigned if you set the deadline under four days away from this day. Are you sure you want to set this deadline?'
+      if (confirm(text) === false) {
+        return
+      }
+    }
+
     try {
       deadlineService.set_deadline({ course_id, date })
       setNewDeadline(true)
