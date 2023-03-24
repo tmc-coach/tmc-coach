0a835f4ce91a783290a7f6b9f51e0f22e99c83e7
János Pap <murmurian@gmail.com>
2023-03-24 11:40:40 +0200

    Modify env variables

--- backend/modules/email.py
+++ backend/modules/email.py
@@ -5,10 +5,9 @@ from flask_mail import Mail
 app = current_app
 
 # configure e-mail settings
-# if you want to use gmail, set MAIL_USE_TLS to True and MAIL_PORT to 587
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = 25
-app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS')
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
