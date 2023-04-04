61949473e5f7550844707334f598698a5d6ff68b
karppienkingi <mirka.vaananen@gmail.com>
2023-04-04 19:00:24 +0300

    add loading effect

--- frontend/src/routes/ProfilePage.js
+++ frontend/src/routes/ProfilePage.js
@@ -3,7 +3,7 @@
 //import { Navigate } from 'react-router-dom'
 import authService from '../services/auth'
 import { useState, useEffect } from 'react'
-//import Loading from '../components/Loading'
+import Loading from '../components/Loading'
 import profile from '../assets/profile.png'
 
 
@@ -12,21 +12,27 @@ const Profile = () => {
 
   useEffect(() => {
     authService.get_user_email().then(email => setEmail(email))
+    console.log(email.user_email)
   }, [])
 
   return (
     <>
-      <div className='main container container-fluid'>
-        <h1>My Account</h1>
-        <div className='flex justify-center items-center py-10'>
-          <img width={100} height={60} src={profile}/>
-          <div className='p-2 font-small text-left'>
-            <p>E-mail: {email.user_email}</p>
-            <p>Language: english</p>
+      {email.user_email !== undefined ? (
+        <div className='main container container-fluid'>
+          <h1>My Account</h1>
+          <div className='flex justify-center items-center py-10'>
+            <img width={100} height={60} src={profile} />
+            <div className='p-2 font-small text-left'>
+              <p>E-mail: {email.user_email}</p>
+              <p>Language: english</p>
+            </div>
           </div>
+          <h1>My course deadlines</h1>
         </div>
-        <h1>My course deadlines</h1>
-      </div>
+      ) : (
+        <Loading />
+      )
+      }
     </>
   )
 }
