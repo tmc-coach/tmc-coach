e84a378b1d833265ef50e2e5c27fc59f73560e11
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2023-04-04 12:23:46 +0300

    added boolean value about whether late or not

--- backend/modules/points.py
+++ backend/modules/points.py
@@ -3,6 +3,7 @@ import requests
 from datetime import datetime
 def check_points():
     current_date = datetime.now().date()
+    current_date = "2023-04-04"
     results = get_checkpoint_infos(current_date)
     print("sähköpostia varten:")
     for result in results: 
@@ -14,13 +15,15 @@ def check_points():
         checkpoint_percent = result[3]
         deadline = result[6]
         current_points = len(current_points_from_api(course_id, token, user_id))
+        late = False
+        if current_points < target_points:
+            late = True
         print({"email": email, "course_id": course_id, "checkpoint_percent": checkpoint_percent,
                "current_points": current_points, "target_points": target_points,
-               "course_deadline": deadline})
+               "course_deadline": deadline, "late": late})
 def current_points_from_api(course_id, token, user_id):
     points = requests.get(
-        f"https://tmc.mooc.fi/api/v8/courses/{course_id}/users/{user_id}/points",
-        headers={"Accept": "application/json", "Authorization": token},
+        f"https://tmc.mooc.fi/api/v8/courses/{course_id}/users/{user_id}/points"
     )
     return points.json()
     
