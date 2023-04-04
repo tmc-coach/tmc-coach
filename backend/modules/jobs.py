e845924d9223862d6db25b20a3ef61a5c4bcf338
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2023-04-04 16:50:20 +0300

    set scheduler to send checkpoint emails at 10.30

--- backend/modules/jobs.py
+++ backend/modules/jobs.py
@@ -9,10 +9,8 @@ def schedule(app):
     scheduler = BackgroundScheduler()
     scheduler.add_job(do_stuff, "cron", hour=4, minute=20)
     # scheduler.add_job(do_stuff, "interval", seconds=10)
-    scheduler.add_job(
-        get_checkpoints_for_today, "cron", hour=13, minute=37, args=(app,)
-    )
-    scheduler.add_job(send_checkpoint_emails, "interval", minutes=5)
+    scheduler.add_job(send_checkpoint_emails, "cron", hour=10, minute=30, args=(app,))
+    # scheduler.add_job(send_checkpoint_emails, "interval", seconds=10, args=(app,))
     scheduler.start()
 
 
@@ -24,45 +22,34 @@ def do_stuff():
     print("Never gonna say goodbye")
     print("Never gonna tell a lie and hurt you")
 
-
-def get_checkpoints_for_today(app):
+def send_checkpoint_emails(app):
     with app.app_context():
-        today = datetime.now().date()
-        current_checkpoints = checkpoints.query.filter_by(checkpoint_date=today).all()
-        print("Hello checkpoints!")
-        for user_id, checkpoint_percent in current_checkpoints:
-            # Coach those champions to triumph!
-            # I mean, send e-mail and stuff if necessary
-            pass
-
-def send_checkpoint_emails():
-    # current_date = datetime.now().date()
-    current_date = "2023-04-04"
-    results = get_checkpoint_infos(current_date)
-    # print("sähköpostia varten:")
-    for result in results: 
-        user_id = result[2]
-        token = result[1]
-        target_points = result[4]
-        course_id = result[5]
-        email = result[0]
-        checkpoint_percent = result[3]
-        deadline = result[6]
-        current_points = len(current_points_from_api(course_id, user_id))
-        course_name = course_name_from_api(course_id, token)
-        on_schedule = True
-        if current_points < target_points:
-            on_schedule = False
-        send_email(to=email,
-                   on_schedule=on_schedule,
-                   Course_Name=course_name,
-                   Checkpoint_Percentage=checkpoint_percent,
-                   Completed_Points=current_points,
-                   Expected_Points=target_points,
-                   Course_Deadline=deadline)
-        # print({"email": email, "course_name": course_name, "checkpoint_percent": checkpoint_percent,
-        #        "current_points": current_points, "target_points": target_points,
-        #        "course_deadline": deadline, "on_schedule": on_schedule})
+        current_date = datetime.now().date()
+        results = get_checkpoint_infos(current_date)
+        for result in results: 
+            user_id = result[2]
+            token = result[1]
+            target_points = result[4]
+            course_id = result[5]
+            email = result[0]
+            checkpoint_percent = result[3]
+            course_deadline = result[6]
+            course_deadline = course_deadline.strftime('%d.%m.%Y')
+            current_points = len(current_points_from_api(course_id, user_id))
+            course_name = course_name_from_api(course_id, token)
+            on_schedule = True
+            if current_points < target_points:
+                on_schedule = False
+            print(current_points, target_points)
+            print(on_schedule)
+            send_email(app=app,
+                    to=email,
+                    on_schedule=on_schedule,
+                    course_name=course_name,
+                    checkpoint_percent=checkpoint_percent,
+                    current_points=current_points,
+                    target_points=target_points,
+                    course_deadline=course_deadline)
 
 #api calls that are needed for sending email
 def current_points_from_api(course_id, user_id):
