ab62bd7feae93a4c35509baf9c02581504fe820a
karppienkingi <mirka.vaananen@gmail.com>
2023-04-14 12:24:38 +0300

    initial disabled view

--- frontend/src/components/Exercises.js
+++ frontend/src/components/Exercises.js
@@ -4,18 +4,24 @@ const Exercises = ({ exercises, course_id }) => (
 
   <div key={exercises[0].course_id} className='w-full'>
     <h1>{exercises[0].course_title}</h1>
-    {exercises[0].maximum_exercises > -1 ? (
+    {exercises[0].disabled_status === 'enabled' ? (
       <>
-        <div className='text-2xl font-medium pb-5'>
-          Awarded points: {exercises[0].completed_percentage} %
-        </div>
-        <div className="w-full bg-gray-200 rounded-full max-w-2xl mx-auto">
-          <div className="bg-indigo-600 text-xs font-medium text-blue-100 text-center p-1.5 leading-none rounded-full" style={{ width: `${(exercises[0].completed_percentage < 5) ? 5 : exercises[0].completed_percentage}` + '%' }} >{exercises[0].completed_percentage} %</div>
-        </div>
-        <Deadlines course_id={course_id} />
+        {exercises[0].maximum_exercises > -1 ? (
+          <>
+            <div className='text-2xl font-medium pb-5'>
+              Awarded points: {exercises[0].completed_percentage} %
+            </div>
+            <div className="w-full bg-gray-200 rounded-full max-w-2xl mx-auto">
+              <div className="bg-indigo-600 text-xs font-medium text-blue-100 text-center p-1.5 leading-none rounded-full" style={{ width: `${(exercises[0].completed_percentage < 5) ? 5 : exercises[0].completed_percentage}` + '%' }} >{exercises[0].completed_percentage} %</div>
+            </div>
+            <Deadlines course_id={course_id} />
+          </>
+        ) : (
+          <p>No exercises on this course</p>
+        )}
       </>
     ) : (
-      <p>No exercises on this course</p>
+      <p>Disabled</p>
     )}
   </div>
 )
