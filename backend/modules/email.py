407728442881c8dd3b784cea91d06f8f6880b106
János Pap <murmurian@gmail.com>
2023-04-17 16:17:42 +0300

    Add async email

--- backend/modules/email.py
+++ backend/modules/email.py
@@ -1,7 +1,13 @@
 import os
+from threading import Thread
 from flask import render_template
 from flask_mail import Message
 from app import mail
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 
 def send_email(
@@ -14,24 +20,25 @@ def send_email(
     target_points,
     course_deadline,
 ):
-    with app.app_context():
-        if on_schedule:
-            template = "on_schedule"
-            subject = f"{course_name} Checkpoint Update: You're on track!"
-        else:
-            template = "falling_behind"
-            subject = f"{course_name} Checkpoint Update: Let's get back on track!"
-        msg = Message(
-            subject,
-            sender=os.getenv("MAIL_SENDER"),
-            recipients=[to],
-        )
-        msg.body = render_template(
-            template + ".txt",
-            Course_Name=course_name,
-            Checkpoint_Percentage=checkpoint_percent,
-            Completed_Points=current_points,
-            Expected_Points=target_points,
-            Course_Deadline=course_deadline,
-        )
-        mail.send(msg)
+    if on_schedule:
+        template = "on_schedule"
+        subject = f"{course_name} Checkpoint Update: You're on track!"
+    else:
+        template = "falling_behind"
+        subject = f"{course_name} Checkpoint Update: Let's get back on track!"
+    msg = Message(
+        subject,
+        sender=os.getenv("MAIL_SENDER"),
+        recipients=[to],
+    )
+    msg.body = render_template(
+        template + ".txt",
+        Course_Name=course_name,
+        Checkpoint_Percentage=checkpoint_percent,
+        Completed_Points=current_points,
+        Expected_Points=target_points,
+        Course_Deadline=course_deadline,
+    )
+    thread = Thread(target=send_async_email, args=[app, msg])
+    thread.start()
+    return thread
