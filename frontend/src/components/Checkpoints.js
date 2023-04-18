e07c2d1aee4a1cae85e14ba23b12c9cf0236f63d
AnonymousHobbit <30089493+AnonymousHobbit@users.noreply.github.com>
2023-04-18 14:21:41 +0300

    new changes

--- frontend/src/components/Checkpoints.js
+++ frontend/src/components/Checkpoints.js
@@ -5,9 +5,8 @@ const Checkpoints = ({ deadlines }) => (
   <>
     <div className='text-lg font-medium pb-2'>Your checkpoints:</div>
     {deadlines.checkpoints.map(checkpoint => (
-      <div key={checkpoint.id}>
-        <p>{checkpoint.checkpoint_percentage}</p>
-        <p>{checkpoint.checkpoint_date.split(' ')[0]}</p>
+      <div key={checkpoint.id} className="my-3">
+        <p>{checkpoint.checkpoint_percent}%<span className="border-indigo-500 border-solid border-2 mx-2"></span>{checkpoint.checkpoint_date.split(' ')[0]}</p>
       </div>
     ))}
   </>
