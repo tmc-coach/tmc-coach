b355d89c2098be0bd7540c00a579b072e8f97ea6
AnonymousHobbit <30089493+AnonymousHobbit@users.noreply.github.com>
2023-04-18 15:43:39 +0300

    convert date to mroe readable format

--- frontend/src/components/Checkpoints.js
+++ frontend/src/components/Checkpoints.js
@@ -1,15 +1,21 @@
 import React from 'react'
 
+const formatDate = (d) => {
+  const date = new Date(d)
+  return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
+}
 
-const Checkpoints = ({ deadlines }) => (
-  <>
-    <div className='text-lg font-medium pb-2'>Your checkpoints:</div>
-    {deadlines.checkpoints.map(checkpoint => (
-      <div key={checkpoint.id} className="my-3">
-        <p>{checkpoint.checkpoint_percent}%<span className="border-indigo-500 border-solid border-2 mx-2"></span>{checkpoint.checkpoint_date.split(' ')[0]}</p>
-      </div>
-    ))}
-  </>
-)
+const Checkpoints = ({ deadlines }) => {
+  return (
+    <>
+      <div className='text-lg font-medium pb-2'>Your checkpoints:</div>
+      {deadlines.checkpoints.map(checkpoint => (
+        <div key={checkpoint.id} className="my-3">
+          <p>{checkpoint.checkpoint_percent}%<span className="border-indigo-500 border-solid border-2 mx-2"></span>{formatDate(checkpoint.checkpoint_date.split(' ')[0])}</p>
+        </div>
+      ))}
+    </>
+  )
+}
 
 export default Checkpoints
