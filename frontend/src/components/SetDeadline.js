ab6e88cdbbf26d0e7f7627cd884744bd99dcbfed
Tiina <tii.kui12na@gmail.com>
2023-04-23 21:44:13 +0300

    ui fixes

--- frontend/src/components/SetDeadline.js
+++ frontend/src/components/SetDeadline.js
@@ -1,6 +1,7 @@
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
-
+import Loading from '../components/Loading'
+import React, { useState, useEffect } from 'react'
 
 const SetDeadline = ({ date, handleSetDeadline, setDate, message, deadlines, checkpoints, setCheckpoints, frequency, setFrequency }) => {
   const options = [
@@ -19,27 +20,44 @@ const SetDeadline = ({ date, handleSetDeadline, setDate, message, deadlines, che
     setCheckpoints(numberOfCheckpoints)
   }
 
+  const [LoadingSpinner, setLoadingSpinner] = useState(true)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoadingSpinner(false)
+    }, 500)
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [])
+
+
   return (
     <div>
-      {deadlines.length === 0 ? ( <>
-        <div className="my-2 rounded-md border-solid border-4 border-black-800">
+      {LoadingSpinner ? (
+        <Loading />
+      ) : deadlines.length === 0 ? (
+        <>
           <h1>Set a deadline</h1>
-          <p>To schedule the course, first, you need to choose the final date
-            for the deadline from the calendar. Then select amount of checkpoints
-            from the menu. Checkpoints are smaller deadlines for the course and will be
-            distributed automatically based on your preferences. Each
-            checkpoint will include a similar amount of work. TMC Coach will
-            cheer and keep in touch with you by email.
-            Enjoy studying!
+          <p>
+            To schedule the course, first, you need to choose the final date for
+            the deadline from the calendar. Then select amount of checkpoints
+            from the menu. Checkpoints are smaller deadlines for the course and will
+            be distributed automatically based on your preferences. Each checkpoint
+            will include a similar amount of work. TMC Coach will cheer and keep in
+            touch with you by email. Enjoy studying!
           </p>
-        </div>
-      </>
-      ) : ( <>
-        <h1>Set a new deadline</h1>
-        <p>You can change your current deadline for the course by choosing a
-          new date from the calendar and select new checkpoints from the menu.
-        </p>
-      </>
+        </>
+      ) : (
+        deadlines.length !== 0 && (
+          <>
+            <h1>Set a new deadline</h1>
+            <p>
+              You can change your current deadline for the course by choosing a new
+              date from the calendar and select new checkpoints from the menu.
+            </p>
+          </>
+        )
       )}
       {message ? <p className="flex justify-center px-5 my-5">{message}</p> : null}
       <DatePicker
