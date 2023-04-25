775998ddbd4b9f9f5e337b6862d2193e60f7d2c6
minervaana <mirka.vaananen@gmail.com>
2023-04-25 16:35:19 +0300

    checkpoint perfecting

--- frontend/src/components/Deadline.js
+++ frontend/src/components/Deadline.js
@@ -37,7 +37,7 @@ const Deadline = ({ deadlines, onDelete }) => {
       {LoadingSpinner ? (
         <Loading />
       ) : (
-        <div className="order-last my-4 mx-auto h-full max-w-2xl rounded-md border-solid border-4 border-black-800">
+        <div className="order-last my-4 mx-auto h-auto max-w-2xl rounded-md border-solid border-4 border-black-800">
           <p key={deadlines.id} className="flex justify-center px-5 m-5 text-2xl">You have planned to complete this course by {formatDate(deadlines.date.split(' ')[0])}</p>
           {deadlines.checkpoints && <Checkpoints deadlines={deadlines} />}
           <div className="flex justify-center px-5 m-5">
