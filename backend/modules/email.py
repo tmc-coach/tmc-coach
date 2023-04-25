11fbcfba078e431b2406f90d6abf0330b15a68ad
AnonymousHobbit <30089493+AnonymousHobbit@users.noreply.github.com>
2023-04-25 13:52:46 +0300

    small refactoring

--- backend/modules/email.py
+++ backend/modules/email.py
@@ -9,6 +9,14 @@ def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
+def create_msg(template, subject, to, **kwargs):
+    msg = Message(
+        subject,
+        sender=os.getenv("MAIL_SENDER"),
+        recipients=[to],
+    )
+    msg.body = render_template(template + ".txt", **kwargs)
+    return msg
 
 def send_checkpoint_email(
     app,
@@ -27,19 +35,18 @@ def send_checkpoint_email(
     else:
         template = "falling_behind"
         subject = f"{course_name} Checkpoint Update: Let's get back on track!"
-    msg = Message(
-        subject,
-        sender=os.getenv("MAIL_SENDER"),
-        recipients=[to],
-    )
-    msg.body = render_template(
-        template + ".txt",
+
+    msg = create_msg(
+        template=template,
+        subject=subject,
+        to=to,
         Course_Name=course_name,
-        Checkpoint_Percentage=checkpoint_percent,
+        Checkpoint_Percent=checkpoint_percent,
         Completed_Points=current_points,
         Expected_Points=target_points,
         Course_Deadline=course_deadline,
     )
+
     thread = Thread(target=send_async_email, args=[app, msg])
     thread.start()
     return thread
@@ -60,18 +67,17 @@ def send_deadline_email(
     else:
         template = "not_finished"
         subject = f"{course_name} Deadline day: Let's get back on track!"
-    msg = Message(
-        subject,
-        sender=os.getenv("MAIL_SENDER"),
-        recipients=[to],
-    )
-    msg.body = render_template(
-        template + ".txt",
+
+    msg = create_msg(
+        template=template,
+        subject=subject,
+        to=to,
         Course_Name=course_name,
         Completed_Points=current_points,
         Expected_Points=target_points,
         Course_Deadline=course_deadline,
     )
+
     thread = Thread(target=send_async_email, args=[app, msg])
     thread.start()
     return thread
