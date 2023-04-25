6e0e57ee4c35fa8e98b70fd90f2cc8ddf028490b
Tiina <tii.kui12na@gmail.com>
2023-04-25 15:50:47 +0300

    deleted unused route from back and frontend

--- frontend/src/services/courses.js
+++ frontend/src/services/courses.js
@@ -2,12 +2,6 @@ import axios from 'axios'
 
 const baseUrl = process.env.REACT_APP_BACKEND + '/courses'
 
-const get_course_info = async course_id => {
-
-  const response = await axios.get(`${baseUrl}/${course_id}`, { headers: { Authorization: localStorage.getItem('user') } })
-  return response.data
-}
-
 const get_exercises = async course_id => {
 
   const response = await axios.get(`${baseUrl}/${course_id}/exercises`, { headers: { Authorization: localStorage.getItem('user') } })
@@ -15,4 +9,4 @@ const get_exercises = async course_id => {
 
 }
 
-export default { get_exercises, get_course_info }
+export default { get_exercises }
