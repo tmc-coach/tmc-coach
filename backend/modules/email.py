f5736dadd9e08141e44719b0c85fa16c4870baf7
János Pap <murmurian@gmail.com>
2023-05-02 16:47:51 +0300

    Comment email.py

--- backend/modules/email.py
+++ backend/modules/email.py
@@ -6,6 +6,16 @@ from app import mail
 
 
 def send_async_email(app, msg):
+    """Send an email asynchronously
+
+    Args:
+        app (Flask): Flask app
+        msg (Message): Message object
+
+    Returns:
+        None
+    """
+
     with app.app_context():
         mail.send(msg)
 
@@ -14,6 +24,16 @@ def send_email(
     app,
     **kwargs,
 ):
+    """Send an email
+
+    Args:
+        app (Flask): Flask app
+        **kwargs: keyword arguments
+
+    Returns:
+        Thread object
+    """
+    
     msg = Message(
         kwargs["subject"],
         sender=os.getenv("MAIL_SENDER"),
