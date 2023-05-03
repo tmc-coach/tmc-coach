0eae75eb2577b694c9e4002f87a605cba2b600da
Iida Vilkkinen <iida.vilkkinen@gmail.com>
2023-05-03 17:36:46 +0300

    fixed console error

--- frontend/src/components/Checkpoints.js
+++ frontend/src/components/Checkpoints.js
@@ -10,15 +10,17 @@ const Checkpoints = ({ deadlines }) => {
     <>
       <div className='text-lg font-medium pb-2'>Your checkpoints:</div>
       <table className='mx-auto'>
-        {deadlines.checkpoints.map(checkpoint => (
-          <tr key={checkpoint.id}>
-            <td className='p-1'>{checkpoint.checkpoint_percent}%</td>
-            <td><span className="border-indigo-500 border-solid border-2 mx-2"></span></td>
-            <td>{formatDate(checkpoint.checkpoint_date.split(' ')[0])}</td>
-            <td><span className="border-indigo-500 border-solid border-2 mx-2"></span></td>
-            <td className='p-1'>{checkpoint.desired_points} points should be completed</td>
-          </tr>
-        ))}
+        <tbody>
+          {deadlines.checkpoints.map(checkpoint => (
+            <tr key={checkpoint.id}>
+              <td className='p-1'>{checkpoint.checkpoint_percent}%</td>
+              <td><span className="border-indigo-500 border-solid border-2 mx-2"></span></td>
+              <td>{formatDate(checkpoint.checkpoint_date.split(' ')[0])}</td>
+              <td><span className="border-indigo-500 border-solid border-2 mx-2"></span></td>
+              <td className='p-1'>{checkpoint.desired_points} points should be completed</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   )
